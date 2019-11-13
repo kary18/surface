3724659228e8bc5e70229e438ad73ece47dcbfcb
karima <k.combet@hotmail.com>
2019-11-13 08:51:37 +0100

    update file

--- functions.php
+++ functions.php
@@ -1,34 +1,25 @@
 <?php
-
 function afficheEntete()
 {
-    //echo "longueur | largeur | surface \n";
-    printf("| %9s | %9s | %9s |\n", "longueur", "largeur", "surface");
-
+//  echo "  Longueur  |  Largeur  | Surface   |\n";
+  printf("| %9s | %9s | %9s |\n", "Longueur", "Largeur", "Surface");
 }
-
 function afficheLigne($long, $larg)
 {
-    if ( is_numeric($long) && is_numeric($larg) ){
-        printf("| %9.2f | %9.2f | %9.2f |\n", $long, $larg, calculSurface($long,$larg));
-
-    }else {
-        echo "erreur données :". var_dump($long). var_dump($larg) . "\n";
-    }
-
-    //echo "$long | $larg| ".calculSurface($long, $larg). "\n";
-    
-
+  if ( is_numeric($long) && is_numeric($larg) ) {
+    printf("| %9.2f | %9.2f | %9.2f |\n", $long, $larg, calculSurface($long, $larg));
+  } else {
+    echo "Erreur données:". var_dump($long). var_dump($larg) . "\n" ;
+  }
+//  echo " $long | $larg | ".calculSurface($long, $larg)." \n";
 }
-
 function calculSurface($long, $larg)
 {
-    if ( is_numeric($long) && is_numeric($larg) ){
-        $surface = $long * $larg;
-    }else {
-        echo "erreur données :". var_dump($long). var_dump($larg) . "\n";
-    }
-    
-    return($surface);
+  if ( is_numeric($long) && is_numeric($larg) ) {
+    $surface = $long * $larg;
+  } else {
+    echo "Erreur données:". var_dump($long). var_dump($larg) . "\n" ;
+  }
+  return($surface);
 }
 
--- indexphp.php
+++ indexphp.php
@@ -1,11 +1,22 @@
 <?php
 require_once("functions.php");
-
-$chaine = "bonjour";
-$larg = 7;
-$long = 10;
-
-afficheEntete();
-afficheLigne($long, $larg);
-calculSurface($long, $larg);
-afficheLigne($chaine, $long);
+//afficheEntete();
+/*
+afficheLigne(4, 3);
+afficheLigne(2.5, 5);
+afficheLigne(6, 2);
+*/
+$unePiece['piece'] = 'Salle de bain';
+$unePiece['largeur'] = 5;
+$unePiece['longueur'] = 3;
+$unePiece['surface'] = calculSurface($unePiece['largeur'],$unePiece['longueur']);
+print_r($unePiece);
+$uneAutrePiece['piece'] = 'Salon';
+$uneAutrePiece['largeur'] = 7;
+$uneAutrePiece['longueur'] = 8;
+$uneAutrePiece['surface'] = calculSurface($uneAutrePiece['largeur'],$uneAutrePiece['longueur']);
+print_r($uneAutrePiece);
+$listePieces = array();
+$listePieces[0] = $unePiece;
+$listePieces[1] = $uneAutrePiece;
+print_r($listePieces);
